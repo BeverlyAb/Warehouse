c02c67a7d30d6b355554cc77a3b7164bfba2e806
Beverly Abadines <fullstackmachine@dhcp-v068-041.mobile.uci.edu>
2018-04-04 15:51:39 -0700

    working on user parser

---
+++ Mapping_W1/Parser.cpp
@@ -0,0 +1,16 @@
+#include "Parser.h"
+
+/*    string filename;
+    int warehouseWidth;
+    int warehouseHeight;
+*/
+
+void Parser::readFile(string inFile, string outFile)
+{
+
+}
+void Parser::setParam()
+{
+
+}
+
