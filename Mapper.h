5deac552525f39951a2530e98c1fff3cc6a7a9d3
ABADINES <babadine@crystalcove.eecs.uci.edu>
2018-04-11 19:28:17 -0700

    fixed passing position; why does path not save?

--- Mapper.h
+++ Mapper.h
@@ -18,7 +18,12 @@ struct position
 {
     unsigned int x;
     unsigned int y;
-  
+	
+	bool operator==(const position & right) const
+	{	
+		return (x == right.x && y == right.y);
+	}  
+	
 	/*bool operator < (const position & right) const
     {
         return ((y < right.y) || (y == right.y &&  x < right.x));
@@ -40,6 +45,7 @@ class Mapper
     unsigned int height;
     map <position,position> path;
     queue <position> neighbors;
+	map<position,unsigned int> visited;
   
     map<unsigned int, position> stock; //reads via ID
     map<position, unsigned int> shelf; //reads via position and # of items in shelf
@@ -58,14 +64,14 @@ class Mapper
     void validNeighbors(position next);
     
 	int shortest(int label[], bool visited[], position * grid, position cur);
-    void printPath( map<position,unsigned int>, position start, position end);
+    void printPath( map<position,unsigned int> &, position start, position end);
 
 //	bool lessVal(	const pair<position,unsigned int> & left,
 //					const pair<position,unsigned int> & right);
 	//position minVal(const map<position, unsigned int> &);  
     
     //friends
-    friend bool operator==(const position &, const position &);
+   // friend bool operator==(const position &, const position &);
 	friend bool operator<(const position & , const position & );
 
 };
