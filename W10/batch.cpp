1ab9547c422392d2d84226563caa308e13f7983b
Beverly Abadines <fullstackmachine@Richards-MBP.T-mobile.com>
2018-06-09 14:26:13 -0700

    something went wrong. gonna backtrack

--- W10/batch.cpp
+++ W10/batch.cpp
@@ -33,7 +33,26 @@ int main(int argc, char *argv[])
   test.readFile(STOCK, infile);
 
   test.getListItems("warehouse-orders.csv",0);//change from hardcode later 
-  for(int i = 0; i < 100; i++){
+  
+  // int ** arr = NULL;
+  // int n = test.getItems().size() + 1;
+  // arr = (int **)malloc(n* sizeof(int*));
+  //  //Check memory validity
+  // if(arr == NULL)
+  // {
+  // return 1;
+  // }
+
+  // for(int i = 0; i < n; i++){
+  //   arr[i] = (int *)malloc(n* sizeof(int));
+  //    //Check memory validity
+  //   if(arr[i] == NULL)
+  //   {
+  //   return 1;
+  //   }
+  // }
+
+  for(int i = 0; i < 2; i++){
     test.processSingleOrder(i);
     queue<unsigned int> itemList = test.getItems();
     queue<unsigned int> d = test.getItems();
@@ -45,36 +64,46 @@ int main(int argc, char *argv[])
       test.makeRefDP();
       test.preProcess();
     }
-  //    int ** arr = test.makeSubDP();
-  //   int startPos = 0;
-  //   map<int, int> myorder;//ID, index
-  //   myorder.insert(pair<int,int>(startPos, 0)); //include start!
-  //   int size = itemList.size() + 1;           //include start!
+    int ** arr = test.makeSubDP();
+    // for(int i = 0; i < n; i ++){
+    //   for(int j = 0; j < n; j++){
+    //     arr[i][j] = test.makeSubDP()[i][j];
+    //     printf("%i ",test.makeSubDP()[i][j]);
+    //   }
+    //   printf("\n");
+    // }
+    // for(int i = 0; i < n; i++)
+    //   free(arr[i]);
+    // free(arr);
+    int startPos = 0;
+    map<int, int> myorder;//ID, index
+    myorder.insert(pair<int,int>(startPos, 0)); //include start!
+    int size = itemList.size() + 1;           //include start!
     
-  //   for(int i = 1; i < size; i++){
-  //     int ID = itemList.front();
-  //     itemList.pop();
-  //     myorder.insert(pair<int,int>(i,i));
-  //     printf("%i ", ID);
-  //   }
-  // //-------------------------------------------------
-  //   BB branch = BB(myorder, size, arr);
-  //   branch.process();
-  //   unsigned int intermediate[size];
-  //   int IDs[size];
-  //   for(int i = 1; i <size; i++){ //exclude start
-  //   unsigned int ID = d.front();
-  //   //   printf("FRONT %i\n", d.front());
-  //   int itemIndx = test.getDPRef(ID);
-  //   intermediate[i] = itemIndx;
-  //   IDs[i] = ID;
-  // //   printf("intermediate %i , ind = %i\n", ID,intermediate[i]);
-  //   d.pop();
-  // } 
+    for(int i = 1; i < size; i++){
+      int ID = itemList.front();
+      itemList.pop();
+      myorder.insert(pair<int,int>(i,i));
+      printf("%i ", ID);
+    }
+  //-------------------------------------------------
+    BB branch = BB(myorder, size, arr);
+    branch.process();
+    unsigned int intermediate[size];
+    int IDs[size];
+    for(int i = 1; i <size; i++){ //exclude start
+    unsigned int ID = d.front();
+    //   printf("FRONT %i\n", d.front());
+    int itemIndx = test.getDPRef(ID);
+    intermediate[i] = itemIndx;
+    IDs[i] = ID;
+  //   printf("intermediate %i , ind = %i\n", ID,intermediate[i]);
+    d.pop();
+  } 
 
-  // finalOrder = branch.mapBackToItems(intermediate, IDs);
-  // test.setOpt(finalOrder);
-  // test.getPath();
+  finalOrder = branch.mapBackToItems(intermediate, IDs);
+  test.setOpt(finalOrder);
+  test.getPath();
   }
 
   return 0;
